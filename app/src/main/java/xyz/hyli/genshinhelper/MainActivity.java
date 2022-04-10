3a569a6032d2f933e00acf28c913630bc491b9ca
long <1169942595@qq.com>
2022-04-10 16:50:57 +0800

    实现顶栏

--- app/src/main/java/xyz/hyli/genshinhelper/MainActivity.java
+++ app/src/main/java/xyz/hyli/genshinhelper/MainActivity.java
@@ -1,23 +1,31 @@
 package xyz.hyli.genshinhelper;
 
 import android.annotation.SuppressLint;
-import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
 
-public class MainActivity extends Activity {
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.viewpager2.widget.ViewPager2;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Ajiu
+ */
+public class MainActivity extends FragmentActivity {
     @SuppressLint("Range")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        getWindow().setStatusBarColor(Color.TRANSPARENT);
-        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
@@ -30,11 +38,55 @@ public class MainActivity extends Activity {
             editor.apply();
         }
 
-
+        initFragment();
 
     }
 
+    public void initFragment() {
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigation);
 
+        ViewPager2 viewPager = findViewById(R.id.MainActivityViewPager);
+        List<Fragment> fragmentList = new ArrayList<>();
+        fragmentList.add(mFragment.newInstance("概览"));
+        fragmentList.add(mFragment.newInstance("工具"));
+        fragmentList.add(mFragment.newInstance("图鉴"));
+        fragmentList.add(mFragment.newInstance("设置"));
+        mFragmentPagerAdapter mFragmentPagerAdapter = new mFragmentPagerAdapter(getSupportFragmentManager(),getLifecycle(),fragmentList);
+        viewPager.setAdapter(mFragmentPagerAdapter);
+        viewPager.setCurrentItem(0);
+
+        viewPager.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                bottomNavigationView.getMenu().getItem(position).setChecked(true);
+                }
+        });
+
+
+        bottomNavigationView.setItemIconTintList(null);
+        bottomNavigationView.setOnItemSelectedListener(menuItem -> {
+            switch (menuItem.getItemId()) {
+                case R.id.tools:
+                    viewPager.setCurrentItem(1);
+                    break;
+                case R.id.map:
+                    viewPager.setCurrentItem(2);
+                    break;
+                case R.id.settings:
+                    viewPager.setCurrentItem(3);
+                    break;
+                default:
+                    viewPager.setCurrentItem(0);
+            }
+            return true;
+        });
+
+
+    }
+    public void onPageSelected(int position) {
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigation);
+        bottomNavigationView.getMenu().getItem(position).setChecked(true);
+    }
     public void startLoginwebView(View view) {
         startActivity(new Intent(this, LoginWebView.class));
     }
---
+++ app/src/main/java/xyz/hyli/genshinhelper/mFragment.java
@@ -0,0 +1,55 @@
+package xyz.hyli.genshinhelper;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class mFragment extends Fragment {
+    private static final String ARG_VIEW = "概览";
+    private View view;
+    private String mView;
+
+    public mFragment() {
+
+    }
+
+
+    public static mFragment newInstance(String ViewID) {
+        mFragment fragment = new mFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_VIEW, ViewID);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mView = getArguments().getString(ARG_VIEW);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        switch (mView) {
+            case "工具" :
+                view = inflater.inflate(R.layout.fragment_tools, container, false);
+
+                break;
+            case "图鉴" :
+                view = inflater.inflate(R.layout.fragment_map, container, false);
+                break;
+            case "设置" :
+                view = inflater.inflate(R.layout.fragment_settings, container, false);
+                break;
+            default:
+                view = inflater.inflate(R.layout.fragment_main, container, false);
+        }
+        return view;
+    }
+}
